697ad14365e9867737c2d8c59befa558fe76f3c2
Rimaz <Rimaz_97@mail.ru>
2023-09-21 19:04:00 +0400

    исправил год

---
+++ hw-2.js
@@ -0,0 +1,75 @@
+// Задание 1
+let one = 10;
+alert(one);
+let two = 20;
+alert(two);
+
+// Задание 2
+let e = 2007;
+let iphone = `год выпуска первого iPhone, 9 января ${e}`;
+alert(iphone);
+
+// Задание 3
+let name = 'Брендан Эйх или Айк (англ. Brendan Eich /ˈaɪk/) — американский программист, создатель языка программирования JavaScript.';
+alert(name);
+
+// Задание 4
+let num1 = 10;
+let num2 = 2;
+alert(num1+num2);
+alert(num1-num2);
+alert(num1*num2);
+alert(num1/num2);
+
+// Задание 5
+let meaning = 2;
+let result = 2**5;
+alert(result);
+
+// Задание 6
+let a = 9;
+let b = 2;
+alert(a % b);
+
+// Задание 7
+let num = 1;
+num +=5; //num + 5;
+num -=3; //num - 3;
+num *=7; //num * 7;
+num /=3; //num / 3;
+num++; //num + 1;
+num--; //num - 1;
+alert(num);
+
+// Задание 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+// Задание 9.0
+let user = {
+	name: 'Шарик',
+	age: 2,
+	user.isAdmin = true;
+};
+
+
+// Задание 9.1
+user[city of residence] = 'Chelny';
+
+// Задание 9.2
+user.age = 5;
+
+// Задание 9.3
+delete user[city of residence];
+
+// Задание 9.4
+let info = prompt();
+alert(user[info]);
+
+// Задание 10
+let names;
+names = prompt('Введите имя');
+alert('Привет,' + names + '!');
+
+
+
